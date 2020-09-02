9bea4fca06bc32582aef331f0a62e78a13755a01
Megan White <meganluisawhite@gmail.com>
2020-09-01 17:37:22 -0700

    created all artwork route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,5 @@
 class Api::ProductsController < ApplicationController
+  def all_products
+    @all_artworks = Product.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#   rails generate model Product title:string artist:string price:integer image_url:string description:string
+
+# product = Product.new({ title: "The Starry Night", artist: "Vincent van Gogh", price: 1050000, image_url: "https://www.vangoghstudio.com//Files/6/102000/102147/ProductPhotos/Source/307257209.jpg", description: "Oil on Canvas. Created in 1889. This artwork is located at MoMA in New York, USA." })
+product = Product.new({ title: "Kumgang Mountains", artist: "Chong Son", price: 1000000, image_url: "https://i.pinimg.com/originals/06/61/6f/06616fa2e34e85428458dd6b00214d97.jpg", description: "Hanging scroll, ink and colors on paper. Created in 1734. Located at Hoam Art Museum,Kyunggi-Do." })
+product.save
