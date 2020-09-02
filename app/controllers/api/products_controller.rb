56a811dec27b6a86efd45180a44565bf8b487b78
Megan White <meganluisawhite@gmail.com>
2020-09-01 17:46:36 -0700

    added first product path

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,11 @@
 class Api::ProductsController < ApplicationController
   def all_products
     @all_artworks = Product.all
+    render "all_products.json.jb"
+  end
+
+  def first_product
+    @first_product = Product.first
+    render "first_product.json.jb"
   end
 end
