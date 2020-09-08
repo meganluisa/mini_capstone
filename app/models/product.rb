39d765d683455700713b4a302b511955e4ce28e8
Megan White <meganluisawhite@gmail.com>
2020-09-08 16:44:05 -0700

    added validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  validates :title, :artist, :price, presence: true
+  validates :price, numerically: { greater_than: 0 }
+  validates :description, length: { in: 2..500 }
+
   def friendly_updated_at
     updated_at.strftime("%m/%d/%Y %I:%M%p")
   end
