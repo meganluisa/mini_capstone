a879accb80e464e806fab6259d497e516b0b25f4
Megan White <meganluisawhite@gmail.com>
2020-09-10 18:08:58 -0700

    added suppliers model

---
+++ app/controllers/api/suppliers_controller.rb
@@ -0,0 +1,6 @@
+class Api::SuppliersController < ApplicationController
+  def show
+    @supplier = Supplier.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
     delete "/products/:id" => "products#destroy"
+    get "/suppliers/:id" => "suppliers#show"
   end
 end
