68b8072b0c0bf2a304066035293a753f018b4c42
Megan White <meganluisawhite@gmail.com>
2020-09-15 18:10:02 -0700

    create orders method added

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,5 +1,19 @@
 class Api::OrdersController < ApplicationController
   def create
-    order = Order.new({ id: current_user })
+    if current_user
+      # tax = 0.9
+      # subtotal = params[:product_id].price
+      product = Product.find_by(id: params[:product_id])
+      subtotal = product.price * params[:quantity].to_i
+      tax = 0.9
+      total = subtotal + (tax * subtotal)
+      @order = Order.new({ user_id: current_user.id, product_id: params[:product_id], quantity: params[:quantity], subtotal: subtotal, tax: tax, total: total })
+      @order.save
+      if @order.save
+        render "show.json.jb"
+      end
+    else
+      render json: { error: @order.errors.full_messages }
+    end
   end
 end
